073bae2df281e3bfd95d15ac2bd90443bd9a852f
smizrahi <seb.mizrahi@gmail.com>
2026-06-10 11:59:33 +0200

    OP#2417: Calendar - Add TSVECTOR support - move calendar search_vector to tsvector / fulltext

--- tests/test_utils/test_strings.py
+++ tests/test_utils/test_strings.py
@@ -1,6 +1,6 @@
 import pytest
 
-from app.utils.strings import get_domain_from_mail
+from app.utils.strings import get_domain_from_mail, strip_accents
 
 def test_get_domain_from_mail():
     with pytest.raises(ValueError):
@@ -8,3 +8,16 @@ def test_get_domain_from_mail():
     assert get_domain_from_mail("aa") is None
     assert get_domain_from_mail("aa@bb.com") == "bb.com"
     assert get_domain_from_mail("aa@bb@cc") is None
+
+def test_strip_accents_diacritics():
+    assert strip_accents("Réunion Joël") == "reunion joel"
+    assert strip_accents("naïve garçon Crêpe") == "naive garcon crepe"
+
+def test_strip_accents_atomic_letters():
+    assert strip_accents("Søren") == "soren"
+    assert strip_accents("cœur Æther") == "coeur aether"
+    assert strip_accents("Straße") == "strasse"
+    assert strip_accents("Łódź") == "lodz"
+
+def test_strip_accents_leaves_non_latin_untouched():
+    assert strip_accents("Привет 你好 C++ jean@x.com") == "привет 你好 c++ jean@x.com"
